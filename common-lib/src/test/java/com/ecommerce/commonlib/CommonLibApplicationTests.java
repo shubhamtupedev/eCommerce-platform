8c0dcf0b59a60341bad83394212f5358a3544437
Shubham Vilas Tupe <shubhamtupe251997@gmail.com>
2025-02-23 01:16:06 +0530

    common lib

---
+++ common-lib/src/test/java/com/ecommerce/commonlib/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.commonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
